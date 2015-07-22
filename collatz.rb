d481fe131b16c003072da5c328d2b5d012cc6094
Afam Agbodike <pair+afam@fundingcircle.com>
2015-07-21 22:21:12 -0700

    Basic implementation

--- collatz.rb
+++ collatz.rb
@@ -1,4 +1,11 @@
 class Collatz
   def self.calculate_steps(n)
+    return 1 if n == 2
+    case n % 2
+    when 0
+      return self.calculate_steps(n/2) + 1
+    else
+      return self.calculate_steps((3 * n) + 1) + 1
+    end
   end
 end
